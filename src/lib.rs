2bd45e0befc5df075c6f1515b6f9a82a81d2ca14
leruaa <aurelien.catinon@gmail.com>
2024-02-25 09:37:43 +0000

    Add a feature for services

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,8 @@ mod error;
 mod token;
 mod token_client;
 mod token_id;
+
+#[cfg(feature = "services")]
 mod token_service;
 
 pub mod stores;
@@ -14,6 +16,8 @@ pub use error::Error;
 pub use token::Token;
 pub use token_client::TokenClient;
 pub use token_id::TokenId;
+
+#[cfg(feature = "services")]
 pub use token_service::TokenService;
 
 sol!(ERC20Contract, "abi/erc20.json");
