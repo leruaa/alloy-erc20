f5e327a820d5336da8b2c55f2584178133ce9f4c
leruaa <aurelien.catinon@gmail.com>
2024-02-26 21:28:20 +0000

    Allow to add known tokens to a store

--- src/constants/mainnet.rs
+++ src/constants/mainnet.rs
@@ -19,10 +19,34 @@ pub static WETH: Lazy<Token> = Lazy::<Token>::new(|| {
     )
 });
 
+pub static WBTC: Lazy<Token> = Lazy::<Token>::new(|| {
+    Token::new(
+        address!("2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599"),
+        String::from("WBTC"),
+        8,
+    )
+});
+
+pub static USDC: Lazy<Token> = Lazy::<Token>::new(|| {
+    Token::new(
+        address!("A0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48"),
+        String::from("USDC"),
+        6,
+    )
+});
+
 pub static USDT: Lazy<Token> = Lazy::<Token>::new(|| {
     Token::new(
         address!("dAC17F958D2ee523a2206206994597C13D831ec7"),
         String::from("USDT"),
         6,
+    )
+});
+
+pub static DAI: Lazy<Token> = Lazy::<Token>::new(|| {
+    Token::new(
+        address!("6B175474E89094C44Da98b954EedeAC495271d0F"),
+        String::from("DAI"),
+        18,
     )
 });
--- src/stores/mod.rs
+++ src/stores/mod.rs
@@ -10,4 +10,17 @@ pub trait TokenStore {
     fn insert(&mut self, chain_id: u8, token: Arc<Token>);
     fn contains(&self, chain_id: u8, id: TokenId) -> bool;
     fn get(&self, chain_id: u8, id: TokenId) -> Option<Arc<Token>>;
+
+    #[cfg(feature = "known-tokens")]
+    fn insert_known_tokens(&mut self, chain_id: u8) {
+        use crate::mainnet;
+
+        if chain_id == 1 {
+            self.insert(chain_id, Arc::new(mainnet::WETH.to_owned()));
+            self.insert(chain_id, Arc::new(mainnet::WBTC.to_owned()));
+            self.insert(chain_id, Arc::new(mainnet::USDC.to_owned()));
+            self.insert(chain_id, Arc::new(mainnet::USDT.to_owned()));
+            self.insert(chain_id, Arc::new(mainnet::DAI.to_owned()));
+        }
+    }
 }
